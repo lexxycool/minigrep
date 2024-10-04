c59b034b0c152a1ca5fc8995b4bfe123061ba268
lexxycool <maxirexy16@yahoo.com>
2024-10-04 22:54:02 +0300

    committing changes for console search app

---
+++ src/lib.rs
@@ -0,0 +1,67 @@
+use std::{error::Error, fs}; 
+
+pub struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &Vec<String>) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+    
+        Ok(
+            Config {
+                query,
+                file_path,
+            }
+        )
+    }
+
+    pub fn run(&self) -> Result<(), Box<dyn Error>> {
+        let contents = fs::read_to_string(self.config_file_path())?;
+
+        for line in search(&self.query, &contents) {
+            println!("{}", line);            
+        }
+        
+        Ok(())
+    }
+
+    pub fn config_file_path(&self) -> String {
+        self.file_path.to_string()
+    }
+
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result:Vec<&str> = Vec::new();
+    
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    result
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust: 
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    } 
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::{env, fs, process};
-use config::Config;
+use std::{env, process};
+use minigrep::Config;
 
 mod config;
 
@@ -7,17 +7,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     
     let config = Config::build(&args)
-    .unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+    .unwrap_or_else(|_err| {
         process::exit(1);
     });
 
-    println!("Searching for {} in file: {}", config.config_query(), config.config_file_path());
-
-    let contents = fs::read_to_string(config.config_file_path())
-    .expect("should be able to read the file");
-
-    println!("With text:\n {contents}");
+    if let Err(_e) = config.run() {
+        process::exit(1);
+    };
     
 }
 
